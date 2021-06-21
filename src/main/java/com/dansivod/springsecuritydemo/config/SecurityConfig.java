46bcb512ace75129baf06015275e897a5910299d
Danila Sivodedov <dan-sivod@mail.ru>
2021-06-22 01:37:33 +0300

    Add form based authentication

--- src/main/java/com/dansivod/springsecuritydemo/config/SecurityConfig.java
+++ src/main/java/com/dansivod/springsecuritydemo/config/SecurityConfig.java
@@ -1,10 +1,8 @@
 package com.dansivod.springsecuritydemo.config;
 
-import com.dansivod.springsecuritydemo.model.Permission;
 import com.dansivod.springsecuritydemo.model.Role;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
@@ -14,6 +12,7 @@ import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
 
 @Configuration
 @EnableWebSecurity
@@ -23,15 +22,22 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
     @Override
     protected void configure(HttpSecurity http) throws Exception {
         http
-                .csrf().disable()
-                .authorizeRequests()
-                .antMatchers("/").permitAll()
-                .anyRequest()
-                .authenticated()
-                .and()
-                .formLogin()
-                .loginPage("/auth/login").permitAll()
-                .defaultSuccessUrl("/auth/success");
+            .csrf().disable()
+            .authorizeRequests()
+            .antMatchers("/").permitAll()
+            .anyRequest()
+            .authenticated()
+            .and()
+            .formLogin()
+            .loginPage("/auth/login").permitAll()
+            .defaultSuccessUrl("/auth/success")
+            .and()
+            .logout()
+            .logoutRequestMatcher(new AntPathRequestMatcher("/auth/logout","POST"))
+            .invalidateHttpSession(true)
+            .clearAuthentication(true)
+            .deleteCookies("JSESSIONID")
+            .logoutSuccessUrl("/auth/login");
     }
 
     @Bean
